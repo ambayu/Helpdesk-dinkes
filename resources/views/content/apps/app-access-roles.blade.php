5ad1a85cf2668461aabad5ea6b861888e521d8d8
bayuichigo <bayuichigo@gmail.com>
2024-08-19 01:48:10 +0700

    user loop

--- resources/views/content/apps/app-access-roles.blade.php
+++ resources/views/content/apps/app-access-roles.blade.php
@@ -55,6 +55,9 @@
     <!-- Role cards -->
 
     <div class="row g-4">
+        @php
+            $counter = 0;
+        @endphp
         @foreach ($roles as $role)
             <div class="col-xl-4 col-lg-6 col-md-6">
                 <div class="card">
@@ -70,209 +73,217 @@
                                             src="{{ $user->profile_photo_path ? Storage::url($user->profile_photo_path) : asset('assets/img/avatars/1.png') }}"
                                             alt="Avatar">
                                     </li>
-                                @endforeach
+                                    @php
+                                        $counter++;
+                                    @endphp
+
+                                    @if ($counter >= 8)
+                                        ...
+                                    @break
+                                @endif
+                            @endforeach
 
 
 
-                            </ul>
-                        </div>
-                        <div class="d-flex justify-content-between align-items-end">
-                            <div class="role-heading">
-                                <h4 class="mb-1 text-body">{{ $role->name }}</h4>
-                                <a href="javascript:void(0);" data-bs-toggle="modal"
-                                    data-bs-target="#editdRoleModal{{ $role->id }}" class="role-edit-modal"><span>Ubah
-                                        Peran</span></a> |
-
-                                <a href="javascript:void(0);" onclick="deleteRole('{{ $role->id }}')">Hapus</a>
-                                <form id="delete-role-form-{{ $role->id }}"
-                                    action="{{ route('app-access-roles.destroy', $role) }}" method="POST"
-                                    style="display: none;">
-                                    @csrf
-                                    @method('DELETE')
-                                </form>
-
-                            </div>
-                            <a href="javascript:void(0);" class="text-muted"><i
-                                    class="mdi mdi-content-copy mdi-20px"></i></a>
-                        </div>
+                        </ul>
                     </div>
-                </div>
-            </div>
-        @endforeach
+                    <div class="d-flex justify-content-between align-items-end">
+                        <div class="role-heading">
+                            <h4 class="mb-1 text-body">{{ $role->name }}</h4>
+                            <a href="javascript:void(0);" data-bs-toggle="modal"
+                                data-bs-target="#editdRoleModal{{ $role->id }}" class="role-edit-modal"><span>Ubah
+                                    Peran</span></a> |
 
-        <div class="col-xl-4 col-lg-6 col-md-6">
-            <div class="card h-100">
-                <div class="row h-100">
-                    <div class="col-5">
-                        <div class="d-flex align-items-end h-100 justify-content-center">
-                            <img src="{{ asset('assets/img/illustrations/add-new-role-illustration.png') }}"
-                                class="img-fluid" alt="Image" width="70">
+                            <a href="javascript:void(0);" onclick="deleteRole('{{ $role->id }}')">Hapus</a>
+                            <form id="delete-role-form-{{ $role->id }}"
+                                action="{{ route('app-access-roles.destroy', $role) }}" method="POST"
+                                style="display: none;">
+                                @csrf
+                                @method('DELETE')
+                            </form>
+
                         </div>
-                    </div>
-                    <div class="col-7">
-                        <div class="card-body text-sm-end text-center ps-sm-0">
-                            <button data-bs-target="#addRoleModal" data-bs-toggle="modal"
-                                class="btn btn-primary mb-3 text-nowrap add-new-role">Tambah Peran</button>
-                            <p class="mb-0">Tambah Peran, jika peran belum ada</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-        <h4 class="fw-medium mb-1 mt-5">Total pengguna di setiap Role</h4>
-        <p class="mb-0 mt-1">Temukan semua izin dan peran pengguna yang diakses di persusahaanmu.</p>
-
-        <div class="col-12">
-            <!-- Role Table -->
-            <div class="card">
-                <div class="card-datatable table-responsive">
-                    <table class="datatables-users table">
-                        <thead class="table-light">
-                            <tr>
-
-                                <th></th>
-                                <th>Nama Lenkap</th>
-                                <th>Nama Pengguna</th>
-                                <th>Peran</th>
-                                <th>Bidang</th>
-                                <th>Status</th>
-                                <th>Actions</th>
-                            </tr>
-                        </thead>
-                    </table>
-                </div>
-            </div>
-            <!--/ Role Table -->
-        </div>
-    </div>
-    <!--/ Role cards -->
-
-    <!-- Add Role Modal -->
-    @include('_partials/_modals/modal-add-role')
-    <!-- / Add Role Modal -->
-
-    {{-- edit role modal --}}
-
-
-    @foreach ($roles as $role)
-        <div class="modal fade" id="editdRoleModal{{ $role->id }}" tabindex="-1" aria-hidden="true">
-            <div class="modal-dialog modal-lg modal-dialog-centered modal-add-new-role">
-                <div class="modal-content p-3 p-md-5">
-                    <button type="button" class="btn-close btn-pinned" data-bs-dismiss="modal" aria-label="Close"></button>
-                    <div class="modal-body p-md-0">
-                        <div class="text-center mb-4">
-                            <h3 class="role-title mb-2 pb-0">Ubah Peran</h3>
-                            <p>Atur peran permission</p>
-                        </div>
-                        <!-- Add role form -->
-                        <form id="" action="{{ route('app-access-roles.update', $role->id) }}" method="POST"
-                            class="row g-3">
-                            @csrf
-                            @method('PUT')
-                            <div class="col-12 mb-4">
-                                <div class="form-floating form-floating-outline">
-                                    <input type="text" id="modalRoleName" name="role_name" class="form-control"
-                                        placeholder="Enter a role name" tabindex="-1" />
-                                    <label for="modalRoleName">Role Name</label>
-                                </div>
-                            </div>
-                            <div class="col-12">
-                                <h5>Role Permissions</h5>
-                                <!-- Permission table -->
-
-                                <!-- Permission table -->
-                            </div>
-                            <div class="col-12 text-center">
-                                <button type="submit" class="btn btn-primary me-sm-3 me-1">Simpan</button>
-                                <button type="reset" class="btn btn-outline-secondary" data-bs-dismiss="modal"
-                                    aria-label="Close">Batal</button>
-                            </div>
-                        </form>
-                        <!--/ Add role form -->
+                        <a href="javascript:void(0);" class="text-muted"><i
+                                class="mdi mdi-content-copy mdi-20px"></i></a>
                     </div>
                 </div>
             </div>
         </div>
     @endforeach
-    {{-- edit role modal --}}
 
-    {{-- modal change role --}}
+    <div class="col-xl-4 col-lg-6 col-md-6">
+        <div class="card h-100">
+            <div class="row h-100">
+                <div class="col-5">
+                    <div class="d-flex align-items-end h-100 justify-content-center">
+                        <img src="{{ asset('assets/img/illustrations/add-new-role-illustration.png') }}"
+                            class="img-fluid" alt="Image" width="70">
+                    </div>
+                </div>
+                <div class="col-7">
+                    <div class="card-body text-sm-end text-center ps-sm-0">
+                        <button data-bs-target="#addRoleModal" data-bs-toggle="modal"
+                            class="btn btn-primary mb-3 text-nowrap add-new-role">Tambah Peran</button>
+                        <p class="mb-0">Tambah Peran, jika peran belum ada</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 
-    <!-- Add Role Modal -->
-    <div class="modal fade" id="changeRoleModal" tabindex="-1" aria-hidden="true">
+    <h4 class="fw-medium mb-1 mt-5">Total pengguna di setiap Role</h4>
+    <p class="mb-0 mt-1">Temukan semua izin dan peran pengguna yang diakses di persusahaanmu.</p>
+
+    <div class="col-12">
+        <!-- Role Table -->
+        <div class="card">
+            <div class="card-datatable table-responsive">
+                <table class="datatables-users table">
+                    <thead class="table-light">
+                        <tr>
+
+                            <th></th>
+                            <th>Nama Lenkap</th>
+                            <th>Nama Pengguna</th>
+                            <th>Peran</th>
+                            <th>Bidang</th>
+                            <th>Status</th>
+                            <th>Actions</th>
+                        </tr>
+                    </thead>
+                </table>
+            </div>
+        </div>
+        <!--/ Role Table -->
+    </div>
+</div>
+<!--/ Role cards -->
+
+<!-- Add Role Modal -->
+@include('_partials/_modals/modal-add-role')
+<!-- / Add Role Modal -->
+
+{{-- edit role modal --}}
+
+
+@foreach ($roles as $role)
+    <div class="modal fade" id="editdRoleModal{{ $role->id }}" tabindex="-1" aria-hidden="true">
         <div class="modal-dialog modal-lg modal-dialog-centered modal-add-new-role">
             <div class="modal-content p-3 p-md-5">
                 <button type="button" class="btn-close btn-pinned" data-bs-dismiss="modal" aria-label="Close"></button>
                 <div class="modal-body p-md-0">
                     <div class="text-center mb-4">
-                        <h3 class="role-title mb-2 pb-0">Change Role</h3>
-                        <p>Set role permissions</p>
+                        <h3 class="role-title mb-2 pb-0">Ubah Peran</h3>
+                        <p>Atur peran permission</p>
                     </div>
                     <!-- Add role form -->
-
-                    <div class="col-12 mb-4">
-                        <div class="form-floating form-floating-outline">
-                            <select id="roleSelect" name="role_id" class="form-select" aria-label="Choose Plan">
-                                <option value="" selected>Pilih Peran</option>
-                                @foreach ($roles as $role)
-                                    <option value="{{ $role->name }}">{{ $role->name }}</option>
-                                @endforeach
-                            </select>
-                            <label for="roleSelect">Pilih Peran</label>
-
+                    <form id="" action="{{ route('app-access-roles.update', $role->id) }}" method="POST"
+                        class="row g-3">
+                        @csrf
+                        @method('PUT')
+                        <div class="col-12 mb-4">
+                            <div class="form-floating form-floating-outline">
+                                <input type="text" id="modalRoleName" name="role_name" class="form-control"
+                                    placeholder="Enter a role name" tabindex="-1" />
+                                <label for="modalRoleName">Role Name</label>
+                            </div>
                         </div>
-                    </div>
+                        <div class="col-12">
+                            <h5>Role Permissions</h5>
+                            <!-- Permission table -->
 
-                    <div class="col-12 mb-4">
-                        <div id="bidangSelectContainer" style="display: none;"
-                            class="form-floating form-floating-outline mb-4">
-                            <select id="bidangSelect" name="bidang" class="select2 form-select"
-                                data-allow-clear="true">
-                                <option value="" selected>Pilih Bidang</option>
-                                <!-- Tambahkan opsi bidang di sini -->
-                                @foreach ($bidangs as $bidang)
-                                    <option value="{{ $bidang->id }}">{{ $bidang->nama_bidang }}</option>
-                                @endforeach
-                            </select>
-                            <label for="bidangSelect">Pilih Bidang</label>
+                            <!-- Permission table -->
                         </div>
-                    </div>
-
-                    <div class="col-12 text-center">
-                        <button type="button" id="saveRoleBtn" class="btn btn-primary me-sm-3 me-1">Submit</button>
-                        <button type="reset" class="btn btn-outline-secondary" data-bs-dismiss="modal"
-                            aria-label="Close">Cancel</button>
-                    </div>
+                        <div class="col-12 text-center">
+                            <button type="submit" class="btn btn-primary me-sm-3 me-1">Simpan</button>
+                            <button type="reset" class="btn btn-outline-secondary" data-bs-dismiss="modal"
+                                aria-label="Close">Batal</button>
+                        </div>
+                    </form>
                     <!--/ Add role form -->
                 </div>
             </div>
         </div>
     </div>
-    <!--/ Add Role Modal -->
+@endforeach
+{{-- edit role modal --}}
 
-    {{-- end change role --}}
-    <script>
-        function deleteRole(roleId) {
+{{-- modal change role --}}
 
-            Swal.fire({
-                title: 'Are you sure?',
-                text: "You won't be able to revert this!",
-                icon: 'warning',
-                showCancelButton: true,
-                confirmButtonText: 'Yes, delete it!',
-                customClass: {
-                    confirmButton: 'btn btn-primary me-3 waves-effect waves-light',
-                    cancelButton: 'btn btn-outline-secondary waves-effect'
-                },
-                buttonsStyling: false
-            }).then((result) => {
-                if (result.isConfirmed) {
-                    document.getElementById('delete-role-form-' + roleId).submit();
-                }
-            });
-        }
-    </script>
+<!-- Add Role Modal -->
+<div class="modal fade" id="changeRoleModal" tabindex="-1" aria-hidden="true">
+    <div class="modal-dialog modal-lg modal-dialog-centered modal-add-new-role">
+        <div class="modal-content p-3 p-md-5">
+            <button type="button" class="btn-close btn-pinned" data-bs-dismiss="modal" aria-label="Close"></button>
+            <div class="modal-body p-md-0">
+                <div class="text-center mb-4">
+                    <h3 class="role-title mb-2 pb-0">Change Role</h3>
+                    <p>Set role permissions</p>
+                </div>
+                <!-- Add role form -->
+
+                <div class="col-12 mb-4">
+                    <div class="form-floating form-floating-outline">
+                        <select id="roleSelect" name="role_id" class="form-select" aria-label="Choose Plan">
+                            <option value="" selected>Pilih Peran</option>
+                            @foreach ($roles as $role)
+                                <option value="{{ $role->name }}">{{ $role->name }}</option>
+                            @endforeach
+                        </select>
+                        <label for="roleSelect">Pilih Peran</label>
+
+                    </div>
+                </div>
+
+                <div class="col-12 mb-4">
+                    <div id="bidangSelectContainer" style="display: none;"
+                        class="form-floating form-floating-outline mb-4">
+                        <select id="bidangSelect" name="bidang" class="select2 form-select"
+                            data-allow-clear="true">
+                            <option value="" selected>Pilih Bidang</option>
+                            <!-- Tambahkan opsi bidang di sini -->
+                            @foreach ($bidangs as $bidang)
+                                <option value="{{ $bidang->id }}">{{ $bidang->nama_bidang }}</option>
+                            @endforeach
+                        </select>
+                        <label for="bidangSelect">Pilih Bidang</label>
+                    </div>
+                </div>
+
+                <div class="col-12 text-center">
+                    <button type="button" id="saveRoleBtn" class="btn btn-primary me-sm-3 me-1">Submit</button>
+                    <button type="reset" class="btn btn-outline-secondary" data-bs-dismiss="modal"
+                        aria-label="Close">Cancel</button>
+                </div>
+                <!--/ Add role form -->
+            </div>
+        </div>
+    </div>
+</div>
+<!--/ Add Role Modal -->
+
+{{-- end change role --}}
+<script>
+    function deleteRole(roleId) {
+
+        Swal.fire({
+            title: 'Are you sure?',
+            text: "You won't be able to revert this!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonText: 'Yes, delete it!',
+            customClass: {
+                confirmButton: 'btn btn-primary me-3 waves-effect waves-light',
+                cancelButton: 'btn btn-outline-secondary waves-effect'
+            },
+            buttonsStyling: false
+        }).then((result) => {
+            if (result.isConfirmed) {
+                document.getElementById('delete-role-form-' + roleId).submit();
+            }
+        });
+    }
+</script>
 
 
 @endsection

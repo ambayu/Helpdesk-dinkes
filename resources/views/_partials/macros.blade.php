6e1893cf95b31044bdbcc854c22dd4ba6ed2f8be
bayuichigo <bayuichigo@gmail.com>
2024-12-10 14:47:40 +0700

    ohpede

--- resources/views/_partials/macros.blade.php
+++ resources/views/_partials/macros.blade.php
@@ -3,6 +3,6 @@
     $withbg = $withbg ?? '#666cff';
 @endphp
 <span>
-    <img width="170px" class="" src="{{ asset('assets/img/branding/logo diskominfo helpdesk - 2.png') }}"
+    <img width="170px" class="" src="{{ asset('assets/img/branding/logo diskominfo helpdesk copy.png') }}"
         alt="">
 </span>
